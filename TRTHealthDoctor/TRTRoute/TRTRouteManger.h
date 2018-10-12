d7e90a7326ec3ec0c002636a7787d1c42eac9469
zhoupengju <1321932448@qq.com>
2018-10-12 15:34:25 +0800

    添加路由文件

---
+++ TRTHealthDoctor/TRTRoute/TRTRouteManger.h
@@ -0,0 +1,17 @@
+//
+//  TRTRouteManger.h
+//  TRTHealthDoctor
+//
+//  Created by 补骨脂的Mac on 2018/10/12.
+//  Copyright © 2018 zhoupengju. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TRTRouteManger : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TRTHealthDoctor/TRTRoute/TRTRouteManger.m
@@ -0,0 +1,13 @@
+//
+//  TRTRouteManger.m
+//  TRTHealthDoctor
+//
+//  Created by 补骨脂的Mac on 2018/10/12.
+//  Copyright © 2018 zhoupengju. All rights reserved.
+//
+
+#import "TRTRouteManger.h"
+
+@implementation TRTRouteManger
+
+@end
